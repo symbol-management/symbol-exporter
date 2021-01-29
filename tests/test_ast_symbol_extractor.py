b8a5166ede3f1a7c4a974bfb696b4b9e4abd02fb
Christopher J. Wright <cjwright4242@gmail.com>
2021-01-29 18:30:07 -0500

    Add CI and tests

---
+++ tests/test_ast_symbol_extractor.py
@@ -0,0 +1,48 @@
+import ast
+
+from symbol_exporter.ast_symbol_extractor import SymbolFinder
+
+
+def test_from_import_attr_access():
+    code = """
+from abc import xyz
+
+def f():
+    return xyz.i
+"""
+    tree = ast.parse(code)
+    z = SymbolFinder()
+    z.visit(tree)
+    assert z.aliases == {'xyz': 'abc.xyz'}
+    assert z.imported_symbols == ['abc.xyz']
+    assert z.used_symbols == {'abc.xyz.i'}
+
+
+def test_alias_import():
+    code = """
+from abc import xyz as l
+
+def f():
+    return l.i
+"""
+    tree = ast.parse(code)
+    z = SymbolFinder()
+    z.visit(tree)
+    assert z.aliases == {'xyz': 'abc.xyz', 'l': 'xyz'}
+    assert z.imported_symbols == ['abc.xyz']
+    assert z.used_symbols == {'abc.xyz.i'}
+
+
+def test_calls():
+    code = """
+import numpy as np
+
+def f():
+    return np.ones(np.twos().three)
+"""
+    tree = ast.parse(code)
+    z = SymbolFinder()
+    z.visit(tree)
+    assert z.aliases == {'np': 'numpy'}
+    assert z.imported_symbols == ['numpy']
+    assert z.used_symbols == {'numpy.ones', 'numpy.twos'}
