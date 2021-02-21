3612d8e19327fb97864d78819e5786e562b4a539
Chris Burr <christopher.burr@cern.ch>
2021-02-21 22:06:31 +0100

    Extract more objects from Python .so files and add basic tests

--- symbol_exporter/python_so_extractor.py
+++ symbol_exporter/python_so_extractor.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import argparse
 from dataclasses import dataclass
 import re
@@ -5,6 +6,7 @@ import struct
 from typing import Optional
 import logging
 from pprint import pprint
+from functools import lru_cache
 
 import pwn
 
@@ -12,13 +14,15 @@ ASM_PATTERN = re.compile(r"^\s+([0-9a-f]+):\s+([0-9a-f]{2}(?: [0-9a-f]{2})*)(?:\
 PYMODULE_STRUCT = struct.Struct("x"*40 + "LLlL")
 PYMETHOD_STRUCT = struct.Struct("LLixxxxL")
 
-logger = logging.getLogger(__name__)
+import colorlog
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+	'%(log_color)s%(levelname)s:%(name)s:%(message)s'))
+logger = logging.getLogger("pyso_extract")
+handler.setLevel(logging.DEBUG)
 logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+logger.addHandler(handler)
 
 
 @dataclass
@@ -45,6 +49,12 @@ class AssemblyInstruction:
         return self.loc_to_symbol.get(self.address)
 
 
+class Tracers(Enum):
+    MODULE = "MODULE"
+    SUBMODULE = "SUBMODULE"
+    MODULE_DICT = "MODULE_DICT"
+
+
 
 def initfunc_name(name):
     # See https://docs.python.org/3/extending/building.html#c.PyInit_modulename
@@ -55,10 +65,10 @@ def initfunc_name(name):
     return 'PyInit' + suffix.decode("ascii")
 
 
-def parse_create(elf, call: AssemblyInstruction, most_recent: dict, results: dict):
-    module = most_recent["lea"]
+def parse_create(elf, call: AssemblyInstruction, registers: dict, results: dict):
+    module = registers["rdi"]
 
-    module_struct_data = elf.read(int(module.address, base=16), PYMODULE_STRUCT.size)
+    module_struct_data = elf.read(int(module, base=16), PYMODULE_STRUCT.size)
     logger.debug("parse_module: module_struct_data is %s", list(map(hex, module_struct_data)))
     name, docs, size, methods_idx = PYMODULE_STRUCT.unpack(module_struct_data)
     assert name != 0, name
@@ -77,16 +87,16 @@ def parse_create(elf, call: AssemblyInstruction, most_recent: dict, results: dic
         while True:
             data = elf.read(methods_idx + offset, PYMETHOD_STRUCT.size)
             offset += PYMETHOD_STRUCT.size
-            if all(d == 0 for d in data):
-                break
             method_name, method_func, method_flags, method_doc = PYMETHOD_STRUCT.unpack(data)
+            if method_name == 0:
+                break
             method_name = elf.string(method_name).decode()
             if method_doc == 0:
                 method_doc = None
             else:
                 method_doc = elf.string(method_doc).decode()
             methods.append({"name": method_name, "docstring": method_doc})
-            logger.debug(
+            logger.info(
                 "parse_create: Found %s in %s at %s",
                 method_name, name, hex(method_func)
             )
@@ -95,18 +105,51 @@ def parse_create(elf, call: AssemblyInstruction, most_recent: dict, results: dic
     results["docstring"] = docs
     results["methods"].extend(methods)
 
+    registers["rax"] = Tracers.MODULE
 
-def parse_add_object(elf, call: AssemblyInstruction, most_recent: dict, results: dict):
-    object_name = elf.string(int(most_recent["lea"].address, 16)).decode()
-    results["objects"].append({"name": object_name})
-    logger.debug("parse_add_object: Found %s", object_name)
+
+def parse_new_submodule(elf, call: AssemblyInstruction, registers: dict, results: dict):
+    if call.symbol_name == "PyModule_New":
+        object_name = elf.string(int(registers["rdi"], 16)).decode()
+    else:
+        raise NotImplementedError(call.symbol_name)
+    logger.info("parse_new_submodule: Found submodule %s", object_name)
+    registers["rax"] = Tracers.SUBMODULE
+
+
+def parse_add_object(elf, call: AssemblyInstruction, registers: dict, results: dict):
+    if registers["rdi"] == Tracers.MODULE:
+        object_name = elf.string(int(registers["rsi"], 16)).decode()
+        results["objects"].append({"name": object_name})
+        logger.info("parse_add_object: Found %s", object_name)
+    else:
+        logger.warning("Called parse_add_object with rdi=%s", registers["rdi"])
+
+
+def PyModule_GetDict(elf, call: AssemblyInstruction, registers: dict, results: dict):
+    if registers["rdi"] == Tracers.MODULE:
+        registers["rax"] = Tracers.MODULE_DICT
+    else:
+        registers["rax"] = None
+
+
+def PyDict_SetItemString(elf, call: AssemblyInstruction, registers: dict, results: dict):
+    if registers["rdi"] == Tracers.MODULE_DICT:
+        object_name = elf.string(int(registers["rsi"], 16)).decode()
+        logger.info("Found call %s(module.__dict__, %s, %s)", call.symbol_name, object_name, registers["rdx"])
+        results["objects"] += [{"name": object_name}]
 
 
 KNOWN_SYMBOLS = {
     "PyModule_Create2": parse_create,
     "PyModuleDef_Init": parse_create,
+    "PyModule_New": parse_new_submodule,
+    "PyModule_NewObject": parse_new_submodule,
     "PyModule_AddObject": parse_add_object,
     "PyModule_AddIntConstant": parse_add_object,
+    "PyModule_AddStringConstant": parse_add_object,
+    "PyModule_GetDict": PyModule_GetDict,
+    "PyDict_SetItemString": PyDict_SetItemString,
 }
 
 
@@ -116,46 +159,102 @@ def parse_file(filename, module_name):
     # Find the locations of relaxant functions in the file
     loc_to_symbol = {}
     for symbol_name, symbol_loc in elf.symbols.items():
+        if symbol_name.startswith(("got.", "plt.")):
+            continue
         symbol_loc = hex(symbol_loc)
         logger.debug("Found symbol for %s at %s", symbol_name, symbol_loc)
         loc_to_symbol[symbol_loc] = symbol_name
 
     # Disassemble the PyInit function of the module
-    init_func = initfunc_name(module_name)
-    init_asm = elf.functions[init_func].disasm()
-    instructions = []
-    logger.debug("Disassembled initialisation function (%s):", init_func)
-    for line in init_asm.split("\n"):
-        match = ASM_PATTERN.fullmatch(line)
-        assert match, line
-        instruction = AssemblyInstruction(loc_to_symbol, *match.groups())
-        instructions.append(instruction)
-        logger.debug(instruction)
-
-    # Parse the assembly calling the parser functions KNOWN_SYMBOLS when needed
-    most_recent = {}
     results = {
         "methods": [],
         "objects": [],
     }
-    for instruction in instructions:
-        most_recent[instruction.instruction] = instruction
-        if instruction.instruction != "call":
-            continue
-        symbol_name = loc_to_symbol.get(instruction.address)
-        if symbol_name in KNOWN_SYMBOLS:
-            KNOWN_SYMBOLS[symbol_name](elf, instruction, most_recent, results)
+    emulate(elf, loc_to_symbol, initfunc_name(module_name), results)
 
     return results
+
+
+disassembled_cache = {}
+
+def disassemble_symbol(elf, loc_to_symbol: dict, function_name: str):
+    global disassembled_cache
+    if function_name not in disassembled_cache:
+        init_asm = elf.functions[function_name].disasm()
+        instructions = []
+        logger.debug("Disassembled function (%s):", function_name)
+        for line in init_asm.split("\n"):
+            match = ASM_PATTERN.fullmatch(line)
+            assert match, line
+            instruction = AssemblyInstruction(loc_to_symbol, *match.groups())
+            instructions.append(instruction)
+            logger.debug(instruction)
+        disassembled_cache[function_name] = instructions
+    return disassembled_cache[function_name]
+
+
+def emulate(elf, loc_to_symbol, function_name, results, registers=None):
+    if registers is None:
+        registers = {}
+    instructions = disassemble_symbol(elf, loc_to_symbol, function_name)
+    stack = []
+
+    # Parse the assembly calling the parser functions in KNOWN_SYMBOLS when needed
+    for instruction in instructions:
+        logger.debug("Executing: %r", instruction)
+        if instruction.instruction in ["lea"]:
+            destination, source = map(str.strip, instruction.args.split(","))
+            registers[destination] = instruction.address
+
+        elif instruction.instruction in ["mov"]:
+            destination, source = map(str.strip, instruction.args.split(","))
+            
+            if match := re.fullmatch(r"[DQ]WORD PTR \[([a-z0-9]{3})(\+0x0)?\]", destination):
+                dest, offset = match.groups()
+                destination = dest
+
+            if instruction.address:
+                registers[destination] = instruction.symbol_name
+            else:
+                registers[destination] = registers.get(source)
+
+        elif instruction.instruction in ["push"]:
+            source, = map(str.strip, instruction.args.split(","))
+            stack += [registers.get(source)]
+
+        elif instruction.instruction in ["pop"]:
+            destination, = map(str.strip, instruction.args.split(","))
+            if stack:
+                registers[destination] = stack.pop()
+            else:
+                logger.warning("Tried to pop but the stack is empty!?")
+
+        elif instruction.instruction in ["call", "jmp"]:
+            if instruction.address:
+                address = instruction.address
+            else:
+                address = instruction.args
+            symbol_name = loc_to_symbol.get(address)
+            logger.debug("Calling %s (%s) with register state:\n    %r", address, symbol_name, registers)
+            if symbol_name in KNOWN_SYMBOLS:
+                KNOWN_SYMBOLS[symbol_name](elf, instruction, registers, results)
+            elif symbol_name in elf.functions:
+                emulate(elf, loc_to_symbol, symbol_name, results, registers)
+            else:
+                logger.debug("Skipping call to %s (%s) as function definition is not available", address, symbol_name)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
     parser.add_argument("module_name")
+    parser.add_argument("--print", action="store_true")
     parser.add_argument("--compare")
     args = parser.parse_args()
     results = parse_file(args.filename, args.module_name)
+
+    if args.print:
+        pprint(results)
 
     if args.compare:
         import importlib
