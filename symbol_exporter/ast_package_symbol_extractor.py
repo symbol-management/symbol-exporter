002191cc5722e576fc81c733d36b5d1536dd1cab
Daniel Alberto Canas <daniel.canas00@gmail.com>
2021-03-25 22:48:47 -0400

    :shirt: Make flake8 happy

--- symbol_exporter/ast_package_symbol_extractor.py
+++ symbol_exporter/ast_package_symbol_extractor.py
@@ -2,7 +2,12 @@ import ast
 from pathlib import Path
 from typing import Union
 
-from symbol_exporter.ast_symbol_extractor import SymbolFinder, is_relative_import, SymbolType, is_relative_star_import
+from symbol_exporter.ast_symbol_extractor import (
+    SymbolFinder,
+    is_relative_import,
+    SymbolType,
+    is_relative_star_import,
+)
 from symbol_exporter.db_access_model import make_json_friendly
 
 
@@ -48,7 +53,9 @@ def remove_shadowed_relative_imports(package_symbols: dict):
 
 def deref_star(package_symbols: dict) -> dict:
     ret = {}
-    star_imports = ((k, v) for k, v in package_symbols.items() if is_relative_star_import(v))
+    star_imports = (
+        (k, v) for k, v in package_symbols.items() if is_relative_star_import(v)
+    )
     for k, v in star_imports:
         imports = v["data"]["imports"]
         for rel_import in imports:
@@ -57,30 +64,41 @@ def deref_star(package_symbols: dict) -> dict:
             print(f"Looking at symbol {k} with data {v} which shadows: {shadows}")
             for symbol in package_symbols:
                 print(f"looking at symbol {symbol}")
-                if symbol.startswith(shadows) and package_symbols[symbol]["type"] not in {SymbolType.PACKAGE,
-                                                                                          SymbolType.MODULE,
-                                                                                          SymbolType.STAR_IMPORT,
-                                                                                          SymbolType.RELATIVE_STAR_IMPORT}:
+                if symbol.startswith(shadows) and package_symbols[symbol][
+                    "type"
+                ] not in {
+                    SymbolType.PACKAGE,
+                    SymbolType.MODULE,
+                    SymbolType.STAR_IMPORT,
+                    SymbolType.RELATIVE_STAR_IMPORT,
+                }:
                     print(f"found symbol {symbol} for shadow {shadows}")
                     s: str = symbol.removeprefix(f"{shadows}")
                     new_symbol = f"{namespace}{s}"
                     # print(f"adding {new_symbol} as star imported symbol")
                     print(f"looking for v {package_symbols[symbol]}")
                     if is_relative_import(package_symbols[symbol]):
-                        dereferenced_shadows = package_symbols[symbol]['data']['shadows']
+                        dereferenced_shadows = package_symbols[symbol]["data"][
+                            "shadows"
+                        ]
                         # print(f"rel import shadows {dereferenced_shadows}")
                         if new_symbol != dereferenced_shadows:
                             # do not add new symbol if same, it is circular reference
-                            print(f"adding symbol {new_symbol} shadowing {dereferenced_shadows}")
+                            print(
+                                f"adding symbol {new_symbol} shadowing {dereferenced_shadows}"
+                            )
                         else:
-                            print(f"found circular reference: {new_symbol} is already in volume of {rel_import['module']},"
-                                  f" it is also exposed through import {shadows}.*")
+                            print(
+                                f"found circular reference: {new_symbol} is already in volume of "
+                                f"{rel_import['module']}, it is also exposed through import {shadows}.*"
+                            )
                     else:
                         print(f"adding symbol {new_symbol} shadowing {symbol}")
             data = dict(v, data=dict(shadows=shadows))
             ret[new_symbol] = data
         # ret[new_symbol] = v
     return ret
+
 
 class DirectorySymbolFinder:
     def __init__(self, directory_name: Union[str, Path], parent: str = None):
@@ -189,29 +207,11 @@ expected = {
         "type": SymbolType.RELATIVE_IMPORT,
         "data": {"shadows": "numpy.version"},
     },
-    "numpy.core.*": {
-        "type": SymbolType.STAR_IMPORT,
-        "data": {
-            "imports": {
-                "requests"
-            }
-        }
-    },
-    "numpy.*": {
-        "type": SymbolType.STAR_IMPORT,
-        "data": {
-            "imports": {
-                "requests"
-            }
-        }
-    },
+    "numpy.core.*": {"type": SymbolType.STAR_IMPORT, "data": {"imports": {"requests"}}},
+    "numpy.*": {"type": SymbolType.STAR_IMPORT, "data": {"imports": {"requests"}}},
     "numpy.version.*": {
         "type": SymbolType.STAR_IMPORT,
-        "data": {
-            "imports": {
-                "requests"
-            }
-        }
+        "data": {"imports": {"requests"}},
     },
 }
 
