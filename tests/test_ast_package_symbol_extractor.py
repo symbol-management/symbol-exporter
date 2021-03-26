b3f2b49132d6313308c973d45eef535e8143ac41
Daniel Alberto Canas <daniel.canas00@gmail.com>
2021-03-25 23:32:12 -0400

    Add tests for package handling tests

---
+++ tests/test_ast_package_symbol_extractor.py
@@ -0,0 +1,97 @@
+from symbol_exporter.ast_package_symbol_extractor import DirectorySymbolFinder
+from symbol_exporter.ast_symbol_extractor import SymbolType
+from symbol_exporter.db_access_model import make_json_friendly
+
+expected = {
+    "numpy.version": {"type": SymbolType.MODULE, "data": {}},
+    "numpy.version.get_versions": {"type": SymbolType.FUNCTION, "data": {"lineno": 4}},
+    "numpy": {"type": SymbolType.PACKAGE, "data": {}},
+    # "numpy.__init__.version": {
+    #     "type": "relative-import",
+    #     "data": {"shadows": "version", "level": 1},
+    # },
+    # "numpy.__init__.get_versions": {
+    #     "type": "relative-import",
+    #     "data": {
+    #         "shadows": "version.get_versions",
+    #         "level": 1
+    #     }
+    # },
+    # "numpy.__init__.core": {
+    #     "type": "relative-import",
+    #     "data": {"shadows": "core", "level": 1},
+    # },
+    "numpy.relative.*": {
+        "type": SymbolType.RELATIVE_STAR_IMPORT,
+        "data": {
+            "imports": [{"shadows": "core", "level": 1, "module": "numpy.__init__"}]
+        },
+    },
+    "numpy.core": {"type": SymbolType.PACKAGE, "data": {}},
+    # "numpy.core.__init__.numeric": {
+    #     "type": "relative-import",
+    #     "data": {"shadows": "numeric", "level": 1},
+    # },
+    "numpy.core.relative.*": {
+        "type": SymbolType.RELATIVE_STAR_IMPORT,
+        "data": {
+            "imports": [
+                {"shadows": "numeric", "level": 1, "module": "numpy.core.__init__"}
+            ]
+        },
+    },
+    # "numpy.core.__init__.abs": {
+    #     "type": "import",
+    #     "data": {"shadows": "numeric.absolute"},
+    # },
+    # "numpy.core.__init__.version": {
+    #     "type": "relative-import",
+    #     "data": {"shadows": "version", "level": 2},
+    # },
+    "numpy.core.numeric": {"type": SymbolType.MODULE, "data": {}},
+    "numpy.core.numeric.ones": {"type": SymbolType.FUNCTION, "data": {"lineno": 1}},
+    "numpy.core.numeric.absolute": {
+        "type": SymbolType.FUNCTION,
+        "data": {"lineno": 5, "symbols_in_volume": {"abs": {"line number": [6]}}},
+    },
+    "numpy.get_versions": {
+        "type": SymbolType.RELATIVE_IMPORT,
+        "data": {"shadows": "numpy.version.get_versions"},
+    },
+    "numpy.core.version": {
+        "type": SymbolType.RELATIVE_IMPORT,
+        "data": {"shadows": "numpy.version"},
+    },
+    "numpy.core.abs": {
+        "type": SymbolType.RELATIVE_IMPORT,
+        "data": {"shadows": "numpy.core.numeric.absolute"},
+    },
+    "numpy.core.get_versions": {
+        "type": SymbolType.RELATIVE_IMPORT,
+        "data": {"shadows": "numpy.version.get_versions"},
+    },
+    "numpy.core.alias_get_versions": {
+        "type": SymbolType.RELATIVE_IMPORT,
+        "data": {"shadows": "numpy.version.get_versions"},
+    },
+    "numpy.core.alias_version": {
+        "type": SymbolType.RELATIVE_IMPORT,
+        "data": {"shadows": "numpy.version"},
+    },
+    "numpy.core.*": {"type": SymbolType.STAR_IMPORT, "data": {"imports": {"requests"}}},
+    "numpy.*": {"type": SymbolType.STAR_IMPORT, "data": {"imports": {"requests"}}},
+    "numpy.version.*": {
+        "type": SymbolType.STAR_IMPORT,
+        "data": {"imports": {"requests"}},
+    },
+}
+
+
+def test_deref_star():
+    import json
+    import sys
+
+    dsf = DirectorySymbolFinder("../tests/numpy")
+    symbols = dsf.extract_symbols()
+    json.dump(symbols, sys.stdout, indent=2, default=make_json_friendly)
+    assert symbols == expected
