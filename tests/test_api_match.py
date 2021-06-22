cb6f2cb0cbca7711e776a816a541b14c860eb40f
Wright, CJ <CJ.Wright@citadel.com>
2021-06-22 11:25:58 -0500

    bring test data into line with current data model

--- tests/test_api_match.py
+++ tests/test_api_match.py
@@ -29,10 +29,14 @@ SAMPLE_TABLE = {
 }
 
 
+def get_symbol_table_dummy_func(x):
+return {'symbol table': SAMPLE_TABLE, 'metadata': {}}
+
+
 def test_find_supplying_version_set():
     volume = {"academic.cli", "academic.cli.AcademicError"}
 
-    intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=lambda x: SAMPLE_TABLE)
+    intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=get_symbol_table_dummy_func)
     assert intersection == {
         "academic": {
             "academic-0.5.1-py_0",
@@ -46,7 +50,7 @@ def test_find_supplying_version_set():
 def test_find_supplying_version_multi_pkg_set():
     volume = {"academic.cli", "academic.cli.AcademicError", "zappy"}
 
-    intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=lambda x: SAMPLE_TABLE)
+    intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=get_symbol_table_dummy_func)
     assert intersection == {
         "academic": {
             "academic-0.5.1-py_0",
@@ -65,5 +69,5 @@ def test_find_supplying_version_null_set():
         "academic.cli.clean_bibtex_authors",
     }
 
-    intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=lambda x: SAMPLE_TABLE)
+    intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=get_symbol_table_dummy_func)
     assert intersection == {"academic": set()}
