540158bf37d5405429f965ddc1fc2b11c16adc83
CJ-Wright-Citadel <83364299+CJ-Wright-Citadel@users.noreply.github.com>
2021-07-29 10:10:10 -0400

    add full cycle test for api matching

--- tests/test_api_match.py
+++ tests/test_api_match.py
@@ -46,6 +46,20 @@ SAMPLE_TABLE = {
             {"artifact name": "cchardet/conda-forge/linux-64/cchardet-2.1.1-py27_0", "shadows": "cchardet._cchardet"}
         ],
     },
+        "cchardet": {
+        "cchardet": [{"artifact name": "cchardet/conda-forge/linux-64/cchardet-2.1.1-py27_0"}],
+        "cchardet._cchardet": [
+            {"artifact name": "cchardet/conda-forge/linux-64/cchardet-2.1.1-py27_0", "shadows": "cchardet._cchardet"}
+        ],
+    },
+    "astropy": {
+        "astropy.A": [
+            {"artifact name": "cchardet/conda-forge/linux-64/cchardet-2.1.1-py27_0", "shadows": "astropy.B"}
+        ],
+        "astropy.B": [
+            {"artifact name": "cchardet/conda-forge/linux-64/cchardet-2.1.1-py27_0", "shadows": "astropy.A"}
+        ],
+    }
 }
 
 
@@ -168,5 +182,11 @@ def test_symbol_doesnt_exist_recursion_error():
     volume = {"cchardet._cchardet.detect_with_confidence"}
 
     intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=get_symbol_table_dummy_func)
-    assert intersection == set()
-    assert bad == volume
+    assert intersection == {'cchardet': {'cchardet/conda-forge/linux-64/cchardet-2.1.1-py27_0'}}
+
+
+def test_cycle():
+    volume = {"astropy.A"}
+
+    intersection, bad = find_supplying_version_set(volume, get_symbol_table_func=get_symbol_table_dummy_func)
+    assert intersection == {'astropy': {'cchardet/conda-forge/linux-64/cchardet-2.1.1-py27_0'}}
