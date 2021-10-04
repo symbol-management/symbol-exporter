072d81a61ebc2e8338477c284cafda48862f7c10
Wright, CJ <CJ.Wright@citadel.com>
2021-10-04 14:07:50 -0500

    update tests to use conda version management and or statements

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,45 +1,43 @@
-from packaging.specifiers import SpecifierSet
+from conda.models.version import VersionSpec
 
 from symbol_exporter.tools import find_version_ranges
 
 
-def check_result(specifier, all_versions, acceptable_versions):
-    specifier = SpecifierSet(specifier)
-    assert set(specifier.filter(all_versions)) == set(acceptable_versions)
+def check_result(all_versions, acceptable_versions, expected_range):
+    version_ranges = find_version_ranges(all_versions, acceptable_versions)
+    assert expected_range == version_ranges
+    if expected_range:
+        specifier = VersionSpec(expected_range)
+        assert all(specifier.any_match(v) for v in acceptable_versions)
 
 
 def test_all_versions_acceptable():
-    assert find_version_ranges(["1.0", "1.1", "1.1.1"], ["1.0", "1.1", "1.1.1"]) == ">=1.0,<=1.1.1"
+    check_result(["1.0", "1.1", "1.1.1"], ["1.0", "1.1", "1.1.1"], ">=1.0,<=1.1.1")
 
 
 def test_some_versions_acceptable():
-    assert find_version_ranges(["1.0", "1.1", "1.1.1", "1.1.2"], ["1.0", "1.1", "1.1.1"]) == ">=1.0,<=1.1.1"
+    check_result(["1.0", "1.1", "1.1.1", "1.1.2"], ["1.0", "1.1", "1.1.1"], ">=1.0,<=1.1.1")
 
 
 def test_versions_acceptable_with_island():
-    assert (
-        find_version_ranges(["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3"], ["1.0", "1.1", "1.1.1", "1.1.3"])
-        == ">=1.0,<=1.1.1,1.1.3"
-    )
+    check_result(["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3"], ["1.0", "1.1", "1.1.1", "1.1.3"], ">=1.0,<=1.1.1|1.1.3")
 
 
 def test_versions_acceptable_with_two_ranges():
-    assert (
-        find_version_ranges(
-            ["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3", "1.1.4"], ["1.0", "1.1", "1.1.1", "1.1.3", "1.1.4"]
-        )
-        == ">=1.0,<=1.1.1|>=1.1.3,<=1.1.4"
+    check_result(
+        ["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3", "1.1.4"],
+        ["1.0", "1.1", "1.1.1", "1.1.3", "1.1.4"],
+        ">=1.0,<=1.1.1|>=1.1.3,<=1.1.4",
     )
 
 
 def test_versions_acceptable_with_two_islands():
-    assert (
-        find_version_ranges(
-            ["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3", "1.1.4", "1.1.5"], ["1.0", "1.1", "1.1.1", "1.1.3", "1.1.5"]
-        )
-        == ">=1.0,<=1.1.1,1.1.3,1.1.5"
+    check_result(
+        ["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3", "1.1.4", "1.1.5"],
+        ["1.0", "1.1", "1.1.1", "1.1.3", "1.1.5"],
+        ">=1.0,<=1.1.1|1.1.3|1.1.5",
     )
 
 
 def test_versions_acceptable_with_no_compatible():
-    assert find_version_ranges(["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3", "1.1.4", "1.1.5"], []) == ""
+    check_result(["1.0", "1.1", "1.1.1", "1.1.2", "1.1.3", "1.1.4", "1.1.5"], [], "")
